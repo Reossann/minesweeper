bb6f97b8eeef193ced7678d5a8811580e07d8421
Reossann <reossan.1003@icloud.com>
2025-05-31 01:20:40 +0900

    ボムおきと数字代入完了（次は二つの値を計算値に）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './page.module.css';
-document.oncontextmenu = function () {
-  return false;
-};
+const directions = [
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
 let bombcounts = 0;
 const bom = (b: number[][]) => {
   const newbom = structuredClone(b);
@@ -13,11 +20,13 @@ const bom = (b: number[][]) => {
       if (bombcounts === 10 || newbom[y][x] === 100) {
         break;
       }
-      const ram = Math.floor(Math.random() * 10);
-      if (ram === 1) {
-        newbom[y][x] = 100;
-        bombcounts += 1;
-        console.log(bombcounts);
+      if (newbom[y][x] !== 20) {
+        const ram = Math.floor(Math.random() * 20);
+        if (ram === 1) {
+          newbom[y][x] = 100;
+          bombcounts += 1;
+          console.log(bombcounts);
+        }
       }
     }
   }
@@ -25,9 +34,25 @@ const bom = (b: number[][]) => {
     console.log(80);
     return bom(newbom);
   }
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      let bommmmmmm = 0;
+      if (newbom[y][x] !== 20 && newbom[y][x] !== 100) {
+        for (const [dy, dx] of directions) {
+          if (newbom[y + dy] !== undefined) {
+            if (newbom[y + dy][x + dx] === 100) {
+              bommmmmmm += 1;
+            }
+          }
+        }
+        if (bommmmmmm !== 0) {
+          newbom[y][x] = 11 - 1 * bommmmmmm;
+        }
+      }
+    }
+  }
   return newbom;
 };
-let first = 0;
 export default function Home() {
   const [userinputs, setuserinputs] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -52,13 +77,26 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  type CountMap = Record<number, number>;
+  const flat = userinputs.flat();
+  const counts = flat.reduce<CountMap>((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {} as CountMap);
+  console.log(counts[0]);
+  console.log(8);
+  console.log(userinputs);
 
   const clickHandler = (x: number, y: number) => {
     const newboard = structuredClone(userinputs);
     newboard[y][x] = 20;
     setuserinputs(newboard);
+    if (counts[0] === 81) {
+      setuserinputs(bom(newboard));
+    }
   };
-  const clickleftHandler = (x: number, y: number) => {
+  const clickrightHandler = (x: number, y: number, event: React.MouseEvent) => {
+    event.preventDefault();
     const newboard = structuredClone(userinputs);
     if (newboard[y][x] <= 3) newboard[y][x] += 1;
     if (newboard[y][x] === 3) {
@@ -66,20 +104,7 @@ export default function Home() {
     }
     setuserinputs(newboard);
   };
-  if (first === 0) {
-    first += 1;
-    setuserinputs(bom(bombmap));
-  }
-  type CountMap = Record<number, number>;
-  const flat = bombmap.flat();
-  const counts = flat.reduce<CountMap>((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
-    return acc;
-  }, {} as CountMap);
-
-  console.log(8);
   console.log(userinputs);
-
   return (
     <div className={styles.container}>
       <div className={styles.board}>
@@ -89,7 +114,7 @@ export default function Home() {
               className={styles.cell}
               key={`${x}-${y}`}
               onClick={() => clickHandler(x, y)}
-              onContextMenu={() => clickleftHandler(x, y)}
+              onContextMenu={(event) => clickrightHandler(x, y, event)}
               style={{
                 background: color === 20 ? '#c6c6c6' : '#4c545c',
                 border: color === 20 ? 'none' : '#707880 #222a32 #222a32 #707880',
