015ae8b82a545f4450b4b9242acd16f93e082d00
Reossann <reossan.1003@icloud.com>
2025-06-06 23:04:26 +0900

    　難易度のベースが完成（上級とカスタムを作りに行く）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ const directions = [
   [1, 1],
 ];
 let bombcounts = 0;
-const bom = (b: number[][]) => {
+const bom = (b: number[][], Deka: number, Sumbom: number) => {
   const newbom = structuredClone(b);
-  for (let y = 0; y < 9; y++) {
-    for (let x = 0; x < 9; x++) {
-      if (bombcounts === 10 || newbom[y][x] === 15) {
+  for (let y = 0; y < Deka; y++) {
+    for (let x = 0; x < Deka; x++) {
+      if (bombcounts === Sumbom || newbom[y][x] === 15) {
         break;
       }
       if (newbom[y][x] !== 20) {
@@ -29,11 +29,11 @@ const bom = (b: number[][]) => {
       }
     }
   }
-  if (bombcounts !== 10) {
-    return bom(newbom);
+  if (bombcounts !== Sumbom) {
+    return bom(newbom, Deka, Sumbom);
   }
-  for (let y = 0; y < 9; y++) {
-    for (let x = 0; x < 9; x++) {
+  for (let y = 0; y < Deka; y++) {
+    for (let x = 0; x < Deka; x++) {
       let bommmmmmm = 0;
       if (newbom[y][x] !== 15) {
         for (const [dy, dx] of directions) {
@@ -52,10 +52,10 @@ const bom = (b: number[][]) => {
   return newbom;
 };
 
-const calc = (bombmap: number[][], userinputs: number[][]) => {
+const calc = (bombmap: number[][], userinputs: number[][], Deka: number) => {
   const newcalc = structuredClone(bombmap);
-  for (let y = 0; y < 9; y++) {
-    for (let x = 0; x < 9; x++) {
+  for (let y = 0; y < Deka; y++) {
+    for (let x = 0; x < Deka; x++) {
       if (userinputs[y][x] === 1 || userinputs[y][x] === 2) {
         newcalc[y][x] = userinputs[y][x];
         continue;
@@ -69,10 +69,10 @@ const calc = (bombmap: number[][], userinputs: number[][]) => {
   return newcalc;
 };
 
-const bomcalc = (bombmap: number[][], userinputs: number[][]) => {
+const bomcalc = (bombmap: number[][], userinputs: number[][], Deka: number) => {
   const newcalc = structuredClone(bombmap);
-  for (let y = 0; y < 9; y++) {
-    for (let x = 0; x < 9; x++) {
+  for (let y = 0; y < Deka; y++) {
+    for (let x = 0; x < Deka; x++) {
       newcalc[y][x] = bombmap[y][x] + userinputs[y][x];
       if (newcalc[y][x] === 35) {
         return 5000;
@@ -81,10 +81,10 @@ const bomcalc = (bombmap: number[][], userinputs: number[][]) => {
   }
 };
 
-const remove_all = (calcboard: number[][], userinputs: number[][]) => {
+const remove_all = (calcboard: number[][], userinputs: number[][], Deka: number) => {
   const newuser = structuredClone(userinputs);
-  for (let y = 0; y < 9; y++) {
-    for (let x = 0; x < 9; x++) {
+  for (let y = 0; y < Deka; y++) {
+    for (let x = 0; x < Deka; x++) {
       if (newuser[y][x] === 20) {
         if (23 <= calcboard[y][x] && calcboard[y][x] <= 30) {
           continue;
@@ -101,7 +101,7 @@ const remove_all = (calcboard: number[][], userinputs: number[][]) => {
           if (calcboard[y + dy][x + dx] === 0) {
             newuser[y + dy][x + dx] = 20;
             calcboard[y + dy][x + dx] = 20;
-            return remove_all(calcboard, newuser);
+            return remove_all(calcboard, newuser, Deka);
           }
           if (3 <= calcboard[y + dy][x + dx] && calcboard[y + dy][x + dx] <= 10) {
             newuser[y + dy][x + dx] = 20;
@@ -114,8 +114,18 @@ const remove_all = (calcboard: number[][], userinputs: number[][]) => {
   }
   return calcboard;
 };
+
 let timer = 4;
+
 export default function Home() {
+  const [boardsize, setboardsize] = useState(0);
+
+  const [select, setselect] = useState('');
+
+  const [dekasa, setdekasa] = useState(9);
+
+  const [bombom, setbombom] = useState(10);
+
   const [userinputs, setuserinputs] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -146,6 +156,46 @@ export default function Home() {
     acc[curr] = (acc[curr] || 0) + 1;
     return acc;
   }, {} as CountMap);
+  const f_sele = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setselect(value);
+  };
+  //サイズ変更
+  const sizechange0 = () => {
+    console.log(800);
+    const newboard = [];
+    for (let i = 0; i < 9; i++) {
+      const row = [];
+      for (let j = 0; j < 9; j++) {
+        row.push(0);
+      }
+      newboard.push(row);
+    }
+    flag.current = true;
+    setbombom(10);
+    setdekasa(9);
+    setboardsize(0);
+    setuserinputs(newboard);
+    setbombmap(newboard);
+  };
+
+  const sizechange1 = () => {
+    console.log(800);
+    const newboard = [];
+    for (let i = 0; i < 16; i++) {
+      const row = [];
+      for (let j = 0; j < 16; j++) {
+        row.push(0);
+      }
+      newboard.push(row);
+    }
+    flag.current = true;
+    setbombom(40);
+    setdekasa(16);
+    setboardsize(230);
+    setuserinputs(newboard);
+    setbombmap(newboard);
+  };
 
   const clickHandler = (x: number, y: number) => {
     const newboard = structuredClone(userinputs);
@@ -153,8 +203,8 @@ export default function Home() {
       newboard[y][x] = 20;
     }
     setuserinputs(newboard);
-    if (counts[0] === 81) {
-      setbombmap(bom(newboard));
+    if (counts[0] === dekasa ** 2) {
+      setbombmap(bom(newboard, dekasa, bombom));
     }
   };
   const clickrightHandler = (x: number, y: number, event: React.MouseEvent) => {
@@ -169,7 +219,7 @@ export default function Home() {
     setuserinputs(newboard);
   };
 
-  const boooom = bomcalc(bombmap, userinputs);
+  const boooom = bomcalc(bombmap, userinputs, dekasa);
   if (boooom === 5000) {
     setInterval(() => {
       timer -= 1;
@@ -178,8 +228,9 @@ export default function Home() {
       }
     }, 1000);
   }
-  const C = calc(bombmap, userinputs);
-  const CC = remove_all(C, userinputs);
+  const C = calc(bombmap, userinputs, dekasa);
+  const CC = remove_all(C, userinputs, dekasa);
+  console.log(userinputs);
   const [uptimer, setuptimer] = useState(0);
   const flag = useRef(true);
   useEffect(() => {
@@ -188,18 +239,27 @@ export default function Home() {
         flag.current = false;
         return;
       }
-      const interval = setInterval(() => {
+      setInterval(() => {
         setuptimer((uptimer) => uptimer + 1);
       }, 1000);
     }
   }, [bombmap]);
   console.log(uptimer);
+  console.log(bombmap);
   return (
     <div className={styles.container}>
       <div className={styles.boardP}>
         <div className={styles.boardP2}>
           <div className={styles.boardP3}>
-            <div className={styles.board}>
+            {uptimer}
+            <select onChange={sizechange1}>
+              <option value={1}>1</option>
+              <option>2</option>
+            </select>
+            <div
+              className={styles.board}
+              style={{ width: 270 + boardsize, height: 270 + boardsize }}
+            >
               {CC.map((row, y) =>
                 row.map((color, x) => (
                   <button
